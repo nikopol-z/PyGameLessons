5506ec74b8c0c4385539a4dca937eafff88e7211
Nikolay Zharikov <nikolay.zharikov@mail.ru>
2018-08-03 15:23:17 +0300

    check_for_exit is working

--- game-cars.py
+++ game-cars.py
@@ -2,6 +2,8 @@ import pygame
 import random
 
 
+MAX_X = 1440
+MAX_Y = 900
 
 class Car():
     def __init__(self, x, y):
@@ -9,27 +11,32 @@ class Car():
         self.y = y
 
     def move_car(self):
-        #
+        i = 2
+
 
     def draw_car(self):
-        ##
+        j = 3
 
 
 def check_for_exit():
-
+    global main_running
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
-        elif 
+            #sys.exit()
+            main_running = False
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            #sys.exit()
+            main_running = False
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        if x1 >= 15:
-            x1 -= speed1
-            left = True
-            right = False
-            playerStand1[0] = pygame.transform.rotate(playerStand1[0], -1)
+
+# ---------------------------- MAIN ---------------------------
+pygame.init()
+window = pygame.display.set_mode((MAX_X, MAX_Y), pygame.FULLSCREEN)
+bg_color = (0, 0, 0)
+main_running = True
+
+while main_running:
+    check_for_exit()
+pygame.quit()
